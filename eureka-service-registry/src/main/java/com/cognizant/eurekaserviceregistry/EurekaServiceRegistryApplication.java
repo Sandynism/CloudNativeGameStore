45f4cb76bdba895f4831b200b7336b8f53a8511c
sandynism <31261960+Sandynism@users.noreply.github.com>
2019-10-08 12:02:17 -0400

    adding properties files that have been checked out

--- eureka-service-registry/src/main/java/com/cognizant/eurekaserviceregistry/EurekaServiceRegistryApplication.java
+++ eureka-service-registry/src/main/java/com/cognizant/eurekaserviceregistry/EurekaServiceRegistryApplication.java
@@ -2,9 +2,12 @@ package com.cognizant.eurekaserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceRegistryApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
--- retail-api/src/main/java/com/cognizant/retailapi/RetailApiApplication.java
+++ retail-api/src/main/java/com/cognizant/retailapi/RetailApiApplication.java
@@ -2,8 +2,14 @@ package com.cognizant.retailapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCaching
 public class RetailApiApplication {
 
 	public static void main(String[] args) {
