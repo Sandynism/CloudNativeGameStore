4fca96f89941cf890ba6f5f9c933d07299cf9ec6
sandynism <31261960+Sandynism@users.noreply.github.com>
2019-10-09 17:15:42 -0400

    removing hashes

--- admin-api/src/main/java/com/cognizant/adminapi/util/PasswordConfig.java
+++ admin-api/src/main/java/com/cognizant/adminapi/util/PasswordConfig.java
@@ -16,8 +16,5 @@ public class PasswordConfig {
     }
 }
 
-//Admin - admin || $2a$10$ATyLYRfzbu/daJ.HY0QkgO2L/qzS/a6k6XtMgpfJMaLFHr4/d4Zp6
-//Manager - manager || $2a$10$m8F6rV7YbwpFdXefSRmuD.SX/hgf6kf2NU98Tmie9cOgUJR962Vl.
-//TeamLead - teamlead || $2a$10$jm1AOMnNrNjJugc1squzruzFq8oGDzyB94HpA8zmLzQUacbXWdOWW
-//Employee - employee || $2a$10$bexCKdUGS/RAZI8SAlnEPeY6wIBq/w9Pu7TrNnXJZQWJQ6zC9mtWy
+
 
